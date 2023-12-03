aa02ab88ca5727e3e7c2278ae8f0571cf7ca5366
asmaelhadj <asma.elhadj@insat.ucar.tn>
2023-12-03 23:49:31 +0100

    finalmigration

---
+++ tp4/AspProject/Migrations/20231203221450_tp4final.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AspProject.Migrations
+{
+    public partial class tp4final : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
